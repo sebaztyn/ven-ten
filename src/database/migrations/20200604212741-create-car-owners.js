9c42396c8da231c0c53344668bf633e928b6c6e9
Chima Ekeneme <sebastinechima@gmail.com>
2020-06-04 23:45:15 +0100

    setup project structure and template

---
+++ src/database/migrations/20200604212741-create-car-owners.js
@@ -0,0 +1,54 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('CarOwners', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      first_name: {
+        type: Sequelize.STRING
+      },
+      last_name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      country: {
+        type: Sequelize.STRING
+      },
+      car_model: {
+        type: Sequelize.STRING
+      },
+      car_model_year: {
+        type: Sequelize.STRING
+      },
+      car_color: {
+        type: Sequelize.STRING
+      },
+      gender: {
+        type: Sequelize.STRING
+      },
+      job_title: {
+        type: Sequelize.STRING
+      },
+      bio: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('CarOwners');
+  }
+};
---
+++ src/dbConnection/dbConnection.js
@@ -0,0 +1,14 @@
+/* eslint-disable import/first */
+require("dotenv").config();
+
+const mysql = require("mysql2");
+
+const pool = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PWD,
+});
+
+const promisePool = pool.promise();
+
+module.exports = promisePool;
