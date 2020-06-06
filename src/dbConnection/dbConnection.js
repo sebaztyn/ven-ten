21266a073b439a0af91651f4630dda761c851177
Chima Ekeneme <sebastinechima@gmail.com>
2020-06-06 05:42:53 +0100

    update database connection to cover for production

--- src/dbConnection/dbConnection.js
+++ src/dbConnection/dbConnection.js
@@ -4,10 +4,18 @@
 import "dotenv/config.js";
 import mysql from "mysql2";
 
+const environment = process.env.NODE_ENV;
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PWD,
+  host:
+    environment === "production"
+      ? process.env.DB_HOST_PROD
+      : process.env.DB_HOST,
+  user:
+    environment === "production"
+      ? process.env.DB_USER_PROD
+      : process.env.DB_USER,
+  password:
+    environment === "production" ? process.env.DB_PWD_PROD : process.env.DB_PWD,
   port: 3306,
   connectionLimit: 10,
   waitForConnections: true,
