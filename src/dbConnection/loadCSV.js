689dcf582da64a1757a6d29074b918d22787d1a2
Chima Ekeneme <sebastinechima@gmail.com>
2020-06-06 11:42:48 +0100

    update the script to load the csv file
    - add script to ensure seamless upload in production

--- src/dbConnection/loadCSV.js
+++ src/dbConnection/loadCSV.js
@@ -1,8 +1,9 @@
+/* eslint-disable import/extensions */
 /* eslint-disable no-nested-ternary */
 // eslint-disable-next-line import/extensions
 import "dotenv/config.js";
 import fs from "fs";
-import mysql from "mysql2";
+import mysql from "mysql";
 
 const environment = process.env.NODE_ENV;
 const testDB = process.env.DB_TEST;
@@ -12,47 +13,38 @@ const appRoot = `${process.env.PWD}/src/assets/car_owners_data.csv`;
 const queryString = `LOAD DATA LOCAL INFILE '${appRoot}'
 INTO TABLE Carowners FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\r\n' IGNORE 1 ROWS;`;
 
-const pool = mysql
-  .createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PWD,
-    port: 3306,
-    database:
-      environment === "production"
-        ? productionDB
-        : environment === "test"
-        ? testDB
-        : developmentDB,
-    connectionLimit: 10,
-    waitForConnections: true,
-    queueLimit: 0,
-    keepAliveInitialDelay: 10000,
-    enableKeepAlive: true,
-  })
-  .promise();
+const pool = mysql.createPool({
+  host:
+    environment === "production"
+      ? process.env.DB_HOST_PROD
+      : process.env.DB_HOST,
+  user:
+    environment === "production"
+      ? process.env.DB_USER_PROD
+      : process.env.DB_USER,
+  password:
+    environment === "production" ? process.env.DB_PWD_PROD : process.env.DB_PWD,
+  port: 3306,
+  database:
+    environment === "production"
+      ? productionDB
+      : environment === "test"
+      ? testDB
+      : developmentDB,
+  connectionLimit: 10,
+  waitForConnections: true,
+  queueLimit: 0,
+  keepAliveInitialDelay: 10000,
+  enableKeepAlive: true,
+});
 
 const loadCSV = async () => {
-  return pool
-    .query({
-      sql: queryString,
-      database:
-        environment === "production"
-          ? productionDB
-          : environment === "test"
-          ? testDB
-          : developmentDB,
-      infileStreamFactory: () => fs.createReadStream(appRoot),
-    })
-    .then((result) => {
-      console.log("result :>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> ", result);
-      console.log("CSV file loaded successfully!!");
-    })
-    .then(() => pool.end())
-    .catch((err) => {
-      console.log(err);
-      process.exit(1);
-    });
+  return pool.query(queryString, (error, results, fields) => {
+    if (error) throw error;
+    // console.log("The solution is: ", results[0].solution);
+    console.log(results);
+    pool.end();
+  });
 };
 
 loadCSV();
